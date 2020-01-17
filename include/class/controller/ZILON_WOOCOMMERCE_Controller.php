2c708eb308e8b16187f9e375c5a2b06db46fb821
geeksesi <geeksesi@gmail.com>
2020-01-17 03:47:00 +0330

    sanitize and safe esc

--- include/class/controller/ZILON_WOOCOMMERCE_Controller.php
+++ include/class/controller/ZILON_WOOCOMMERCE_Controller.php
@@ -14,7 +14,7 @@ class ZILON_WOOCOMMERCE_Controller
         add_filter('woocommerce_payment_gateways', [$this, "zilon_gateway_init"]);
         add_action('plugins_loaded', [$this, 'init']);
         add_action('rest_api_init', [$this, "make_back_url"]);
-        add_shortcode('Zilon_io', [$this, "manage_redirect_url"]);
+        add_shortcode('zilon-woocommerce', [$this, "manage_redirect_url"]);
     }
 
 
@@ -88,18 +88,18 @@ class ZILON_WOOCOMMERCE_Controller
             return null;
         }
         $output["ok"] = true;
-        $output["redirectUrl"] = esc_url($this->make_redirect_url($_GET["r_url"], $data["paymentId"]));
+        $output["redirectUrl"] = esc_url($this->make_redirect_url(esc_url_raw($_GET["r_url"]), $data["paymentId"]));
         echo json_encode($output);
         if ($data["status"] != "confirmed") {
             return null;
         }
 
-        $check_payment = $this->api->check_payment($data["paymentId"]);
+        $check_payment = $this->api->check_payment(sanitize_text_field($data["paymentId"]));
         if (!$check_payment || $check_payment != "confirmed") {
             return null;
         }
 
-        $customer_order = new WC_Order($_GET["o_id"]);
+        $customer_order = new WC_Order(sanitize_text_field($_GET["o_id"]));
         $customer_order->payment_complete();
         return null;
     }
@@ -107,20 +107,21 @@ class ZILON_WOOCOMMERCE_Controller
 
     public function manage_redirect_url()
     {
-        if (!isset($_GET["payment_id"]) || !isset($_GET["ok"])) {
+        if (!isset($_GET["payment_id"])) {
             return null;
         }
 
-        if (!is_string($_GET["payment_id"]) || !is_string($_GET["ok"])) {
+        if (!is_string($_GET["payment_id"])) {
             return false;
         }
 
         $payment_data = $this->api->payment_info($_GET["payment_id"]);
-        $payment_data["data"]["p_id"] = $_GET["payment_id"];
+        $payment_data["data"]["p_id"] = sanitize_text_field($_GET["payment_id"]);
         if (!$payment_data) {
             return false;
         }
-        if ($_GET["ok"]) {
+
+        if ($payment_data["data"]["status"] == "confirmed") {
             WC()->cart->empty_cart();
         }
         if ($payment_data["data"]["status"] == "confirmed") {
